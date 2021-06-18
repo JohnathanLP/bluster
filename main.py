d91c43a1b45ddc0e0dd1b53b8373ca2a1954080e
JohnathanLP <johnathanlpowell@gmail.com>
2021-06-18 01:39:55 -0600

    added improved stat parsing and formatting

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import requests
 stats={}
 uuids = [x.split(".")[0] for x in os.listdir("world/stats")]
 
-PULL_USERNAMES = True          # Set false to disable loading usernames from Mojang API - this prevents spamming Mojang servers, and also allows for faster debugging
+PULL_USERNAMES = True           # Set false to disable loading usernames from Mojang API - this prevents spamming Mojang servers, and also allows for faster debugging
+TRIM_NAMESPACES = True          # Removes "minecraft:" from the start of stats and categories, may not be desired for modded worlds
 
 for uuid in uuids:
     stats[uuid] = json.load(open("world/stats/" + uuid + ".json"))
@@ -28,29 +29,33 @@ for uuid in stats:
             if category not in categories:
                 categories.append((group,category))
 
-print(categories)
 
 f = open("output.csv", "w")
 
 f.write(",,")
-for uuid in stats:
-    f.write(stats[uuid]["username"] + ",")
+for c in categories:
+    if TRIM_NAMESPACES:
+        f.write(c[0].split(":")[1] + ",")
+    else:
+        f.write(c[0] + ",")
 f.write("\n")
 
 f.write(",,")
-for uuid in stats:
-    f.write(uuid + ",")
+for c in categories:
+    if TRIM_NAMESPACES:
+        f.write(c[1].split(":")[1] + ",")
+    else:
+        f.write(c[1] + ",")
 f.write("\n")
 
-for c in categories:
-    f.write("{},{},".format(*c))
-
-    for uuid in stats:
+for uuid in stats:
+    f.write(stats[uuid]["username"] + ",")
+    f.write(uuid + ",")
+    for c in categories:
         if c[0] in stats[uuid]["stats"]:
             if c[1] in stats[uuid]["stats"][c[0]]:
                 f.write(str(stats[uuid]["stats"][c[0]][c[1]]))
         f.write(",")
-
     f.write("\n")
 
 f.close()
